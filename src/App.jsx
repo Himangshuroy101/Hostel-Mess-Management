ef275987c662ae3f36c4f603965e487ffc5bd32f
Supriyo Ghoshal <ghoshalsupriyo180@gmail.com>
2025-05-29 00:04:54 +0530

    4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Navbar from './Components/NavBar'
 import Slideshow from './Components/Slideshow'
 import Card from './Components/Card'
 import InfoCard from './Components/InfoCard'
+// import Signup from './auth/Signup'
+import Demo from './Components/Demo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +17,7 @@ function App() {
       <Slideshow/>
       <Card/>
       <InfoCard/>
+      <Demo/>
     </>
   )
 }
